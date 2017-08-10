5fd45b76cbfa9f9b8ad34b21c54e67d3a97d3e83
jimmy <sergii.pryhodko@gmail.com>
2017-08-10 12:26:15 +0300

    mockito

--- home-works/src/main/java/com/sergey/prykhodko/homework8/mockito/SecurityService.java
+++ home-works/src/main/java/com/sergey/prykhodko/homework8/mockito/SecurityService.java
@@ -1,4 +1,24 @@
 package com.sergey.prykhodko.homework8.mockito;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class SecurityService {
+
+
+    public String md5(String string) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        digest.reset();
+        digest.update(string.getBytes());
+
+        byte[] md5String = digest.digest();
+
+        StringBuilder hashedString = new StringBuilder();
+
+        for (byte aMd5String : md5String) {
+            hashedString.append(Integer.toHexString(0xFF & aMd5String));
+        }
+
+        return hashedString.toString();
+    }
 }
